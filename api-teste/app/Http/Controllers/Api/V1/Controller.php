05f55d2a8d4c274428aa137e353462ee60dd0625
João Lapa <joaomanoel1996@hotmail.com>
2020-07-11 19:54:20 -0300

    Modificado local das annotations do Controller meramente informativo da api

--- api-teste/app/Http/Controllers/Api/V1/Controller.php
+++ api-teste/app/Http/Controllers/Api/V1/Controller.php
@@ -1,20 +1,21 @@
 <?php
 
+/**
+ * @OA\Info(
+ *   title="Api Teste",
+ *   description="Api teste para o programa de estágio Aiko",
+ *   version="1.0.0",
+ *   @OA\Contact(
+ *     name="João Lapa",
+ *     email="joaomanoel1996@hotmail.com"
+ *   )  
+ * )
+ * @OA\Server(
+ *   url=L5_SWAGGER_CONST_HOST,
+ *   description="Servidor padrão da api"
+ * )
+ */
 class Controller
 {
-    /**
-     * @OA\Info(
-     *   title="Api Teste",
-     *   description="Api teste para o programa de estágio Aiko",
-     *   version="1.0.0",
-     *   @OA\Contact(
-     *     name="João Lapa",
-     *     email="joaomanoel1996@hotmail.com"
-     *   )  
-     * )
-     * @OA\Server(
-     *   url=L5_SWAGGER_CONST_HOST,
-     *   description="Servidor padrão da api"
-     * )
-     */
+    //
 }
