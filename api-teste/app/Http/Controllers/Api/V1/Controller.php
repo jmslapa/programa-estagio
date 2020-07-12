8e67b82bb3291e5505f85647d9de6aef5a0d763a
João Lapa <joaomanoel1996@hotmail.com>
2020-07-12 00:17:17 -0300

    Alteração na annotation da documentação

--- api-teste/app/Http/Controllers/Api/V1/Controller.php
+++ api-teste/app/Http/Controllers/Api/V1/Controller.php
@@ -3,7 +3,8 @@
 /**
  * @OA\Info(
  *   title="Api Teste",
- *   description="Api teste para o programa de estágio Aiko",
+ *   description="Api teste para o programa de estágio Aiko.
+ *     Importante: Todas as requisições devem ter o cabeçalho Accept setado para receber o mime application/json.",
  *   version="1.0.0",
  *   @OA\Contact(
  *     name="João Lapa",
