1541cb833adec94cf4bfdd7dabb12efd3bdd0a13
João Lapa <joaomanoel1996@hotmail.com>
2020-07-12 00:20:40 -0300

    Criado serviço da entidade Parada e implementados os métodos abstratos da classe Pai newResource e newCollection.

---
+++ api-teste/app/Services/Api/ParadaService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services\Api;
+
+use App\Http\Resources\ParadaCollection;
+use App\Http\Resources\ParadaResource;
+use App\Parada;
+
+/**
+ * Serviço abstrato base para os serviços de entidade
+ */
+class ParadaService extends AbstractService
+{
+    /**
+     * Retorna uma instância de Resource do Model base do serviço.
+     *
+     * @return Resoruce
+     */
+    public function newResource($resource)
+    {
+        return new ParadaResource($resource);
+    }
+
+    /**
+     * Retorna uma instância de ResourceCollection do Model base do serviço.
+     *
+     * @return ResourceCollection
+     */
+    public function newCollection($resource)
+    {
+        return new ParadaCollection($resource);
+    }
+}
