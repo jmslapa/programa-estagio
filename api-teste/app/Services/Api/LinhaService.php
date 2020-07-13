c2ffa0e3cd1fcfef4bfaf5ce22ae053a44024c25
João Lapa <joaomanoel1996@hotmail.com>
2020-07-13 18:24:42 -0300

    Criados métodos newParadaCollection que retorna um ResourceCollection de Parada e removeParadas

--- api-teste/app/Services/Api/LinhaService.php
+++ api-teste/app/Services/Api/LinhaService.php
@@ -2,9 +2,9 @@
 
 namespace App\Services\Api;
 
-use App\Http\Resources\LinhaCollection;
 use App\Http\Resources\LinhaResource;
-use App\Linha;
+use App\Http\Resources\LinhaCollection;
+use App\Http\Resources\ParadaCollection;
 
 /**
  * Serviço abstrato base para os serviços de entidade
@@ -29,5 +29,29 @@ class LinhaService extends AbstractService
     public function newCollection($resource)
     {
         return new LinhaCollection($resource);
+    }
+
+    /**
+     * Retorna uma instância de ResourceCollection do Model base do serviço.
+     *
+     * @return ResourceCollection
+     */
+    public function newParadaCollection($resource)
+    {
+        return new ParadaCollection($resource);
+    }
+
+    /**
+     * Adiciona uma ou mais paradas à linha especificada
+     * $id: Id da linha alvo
+     * $paradas: array com ids das paradas a serem vinculadas à linha
+     * 
+     * @param integer $id
+     * @param array $paradas
+     * @return Model[]
+     */
+    public function removeParadas($id, $paradas = [])
+    {
+        return $this->newParadaCollection($this->repository->removeParadas($id, $paradas));
     }
 }
