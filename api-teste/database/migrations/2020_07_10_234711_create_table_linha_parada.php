2b5674010c5da3e1574222f527d9c97eb2719913
João Lapa <joaomanoel1996@hotmail.com>
2020-07-13 18:25:32 -0300

    Removida constraint composite primary key, uma vez que o eloquent não suporta chaves compostas.

--- api-teste/database/migrations/2020_07_10_234711_create_table_linha_parada.php
+++ api-teste/database/migrations/2020_07_10_234711_create_table_linha_parada.php
@@ -18,8 +18,7 @@ class CreateTableLinhaParada extends Migration
             $table->unsignedBigInteger('parada_id');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-
-            $table->primary('linha_id', 'parada_id');
+            
             $table->foreign('linha_id')->references('id')->on('linhas');
             $table->foreign('parada_id')->references('id')->on('paradas');
         });
