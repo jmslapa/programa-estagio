bfe6d3de8bd7823b1ed2ca65c7f494f9729e1233
João Lapa <joaomanoel1996@hotmail.com>
2020-07-13 02:47:51 +0000

    Sobrescito método insert no LinhaRepository, de forma que agora possa receber um array com os ids das paradas a serem vinculadas à linha e então criar o relacionamento

--- api-teste/app/Repositories/LinhaRepository.php
+++ api-teste/app/Repositories/LinhaRepository.php
@@ -2,10 +2,52 @@
 
 namespace App\Repositories;
 
+use App\Parada;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 /**
  * Repositório da entidade Parada
  */
 class LinhaRepository extends AbstractRepository 
-{
-    //
+{   
+
+    /**
+     * Entidade Parada
+     *
+     * @var Parada
+     */
+    private $parada;
+
+    /**
+     * Retorna uma instância de Repository
+     *
+     * @param string $modelClass
+     */
+    public function __construct(Model $model) 
+    {   
+        $this->model = $model;
+        $this->parada = new Parada();
+    }
+    /**
+     * Cria e persiste um novo registro no banco de dados.
+     * $data: Array com os dados a serem atualizados.
+     *
+     * @param  array  $data
+     * @return Model
+     */
+    public function insert($data)
+    {
+        $result = DB::transaction(function() use($data) {
+            $linha = $this->model->create($data);
+            if(isset($data['paradas']) && count($data['paradas'])) {
+                foreach($data['paradas'] as $p) {
+                    $parada = $this->parada->findOrFail($p);
+                }
+                $linha->paradas()->sync($data['paradas']);
+            }
+            return $linha;
+        });
+        return $result;
+    }
 }
