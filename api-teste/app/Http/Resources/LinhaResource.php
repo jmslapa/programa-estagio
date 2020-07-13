0103b68f7a7aa7819501ebc39e0f6dc72257204e
João Lapa <joaomanoel1996@hotmail.com>
2020-07-13 18:23:18 -0300

    Agora as paradas serão atributos do Resource de linha, desde que as mesmas tenham sido carregadas previamente no modelo.

--- api-teste/app/Http/Resources/LinhaResource.php
+++ api-teste/app/Http/Resources/LinhaResource.php
@@ -37,6 +37,7 @@ class LinhaResource extends JsonResource
         return [            
             'id' => $this->id,
             'name' => $this->name,
+            'paradas' => $this->whenLoaded('paradas'),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
