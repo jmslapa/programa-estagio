82733c22d61310cb03f8718c22e46107be6cd7cb
João Lapa <joaomanoel1996@hotmail.com>
2020-07-13 19:51:07 -0300

    Classe recebeu annotations para a documentação no padrão OpenAPI

--- api-teste/app/Veiculo.php
+++ api-teste/app/Veiculo.php
@@ -5,11 +5,86 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Classe que representa o modelo de veículo
- * @OA/Schema
+ * @OA\Schema(
+ *     title="Veiculo",
+ *     description="model Veiculo",
+ *     @OA\Xml(
+ *         name="Veiculo"
+ *     )
+ * )
  */
 class Veiculo extends Model
 {   
+
+    /**
+     * @OA\Property(
+     *     title="ID",
+     *     description="Identificador único",
+     *     format="int64",
+     *     example=1
+     * )
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @OA\Property(
+     *     title="Name",
+     *     description="Nome do veículo",
+     *     example="Ônibus 48 lugares"
+     * )
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @OA\Property(
+     *     title="Modelo",
+     *     description="Modelo de um veículo",
+     *     example="Marcopolo Ideale 770"
+     * )
+     *
+     * @var string
+     */
+    private $modelo;
+
+    /**
+     * @OA\Property(
+     *     title="Created at",
+     *     description="Data de criação do registro no banco de dados em formato UTC",
+     *     example="2020-07-11 16:30:45",
+     *     format="datetime",
+     *     type="string"
+     * )
+     *
+     * @var \DateTime
+     */
+    private $created_at;    
+
+    /**
+     * @OA\Property(
+     *     title="Updated at",
+     *     description="Data da última atualização do registro no banco de dados em formato UTC",
+     *     example="2020-07-11 16:30:45",
+     *     format="datetime",
+     *     type="string"
+     * )
+     *
+     * @var \DateTime
+     */
+    private $updated_at;
+
+    /**
+     * Atributos que são atribuíveis em massa.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'modelo', 'linha',
+    ];
+
     /**
      * Retorna a linha a qual o veículo integra
      *
