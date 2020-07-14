a8c8f98ef1f189f28b8b5edb70f256944673071f
João Lapa <joaomanoel1996@hotmail.com>
2020-07-13 22:36:15 -0300

    Injeção de dependência de PosicaoVeiculo. Sobrescrita do método insert, agora ao criar um novo veículo, também será criada e vinculada ao mesmo uma PosicaoVeiculo

--- api-teste/app/Repositories/VeiculoRepository.php
+++ api-teste/app/Repositories/VeiculoRepository.php
@@ -2,10 +2,54 @@
 
 namespace App\Repositories;
 
+use App\PosicaoVeiculo;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 /**
  * Repositório da entidade Veiculo
  */
-class VeiculoRepository extends AbstractRepository 
-{
-    //
+class VeiculoRepository extends AbstractRepository
+{   
+    /**
+     * Instância de Entidade PosicaoVeiculo
+     *
+     * @var PosicaoVeiculo
+     */
+    private $posicao;
+
+    /**
+     * Retorna uma instância de Repository
+     *
+     * @param string $modelClass
+     */
+    public function __construct(Model $model)
+    {   
+        $this->model = $model;
+        $this->posicao = new PosicaoVeiculo();
+    }
+
+    /**
+     * Insere um novo registro no banco de dados 
+     * 
+     * @param array  $data
+     *
+     * @return Model
+     */
+    public function insert($data) {
+        
+        $result = DB::transaction(function() use($data) {
+
+            $veiculo = $this->model->create($data);
+            $posicao = [
+                'veiculo_id' => $veiculo->id,
+                'latitude' => $data['latitude'],
+                'longitude' => $data['longitude']
+            ];
+            $this->posicao->create($posicao);
+            return $veiculo;
+        });
+
+        return $result;
+    }
 }
