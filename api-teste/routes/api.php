9fc97a0138995a2ff52872d07022af2179a8fb7b
João Lapa <joaomanoel1996@hotmail.com>
2020-07-14 01:24:58 -0300

    Endpoints redeclarados com novo agrupamento. Declaração dos endpoints de busca

--- api-teste/routes/api.php
+++ api-teste/routes/api.php
@@ -13,32 +13,47 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group([
-    'prefix' => 'v1',
-    'namespace' => 'Api\\V1\\Admin',
-], function() {
+// endpoints api v1
+Route::prefix('v1')->namespace('Api\\V1')->group(function() {
 
-    // endpoints users
-    Route::resource('/users', 'UserController');
-
-    // endpoints paradas 
-    Route::resource('/paradas', 'ParadaController');
-
-    // endpoints linhas 
-    Route::resource('/linhas', 'LinhaController');
-    Route::name('linhas.')->group(function() {
-        Route::delete('/linhas/{id}/paradas', 'LinhaController@removeParadas')->name('removeParadas');
+    // endpoints search
+    Route::prefix('search')->name('search.')->group(function() {
+        
+        // Linhas por parada
+        Route::get('/{parada_id}/linhas-por-parada', 'SearchController@linhasPorParada')
+        ->name('linhasPorParada');
+        // Linhas por parada
+        Route::get('/{linha_id}/veiculos-por-linha', 'SearchController@veiculosPorLinha')
+        ->name('veiculosPorLinha');
     });
 
-    // endpoints de veiculos
-    Route::resource('/veiculos', 'VeiculoController');
+    // endpoints restritos
+    Route::namespace('Admin')->group(function() {
 
-    // endpoints de posicaoVeiculos
-    Route::prefix('posicao-veiculos')->name('posicaoVeiculos.')->group(function() {
-        Route::get('/', 'PosicaoVeiculoController@index')->name('index');
-        //Route::post('/', 'PosicaoVeiculoController@store')->name('store');
-        Route::get('/{id}', 'PosicaoVeiculoController@show')->name('show');        
-        Route::put('/{id}', 'PosicaoVeiculoController@update')->name('update');                
-        //Route::delete('/{id}', 'PosicaoVeiculoController@destroy')->name('destroy');
+        // endpoints users
+        Route::resource('/users', 'UserController');
+
+        // endpoints paradas 
+        Route::resource('/paradas', 'ParadaController');
+
+        // endpoints linhas 
+        Route::resource('/linhas', 'LinhaController');
+        Route::name('linhas.')->group(function() {
+            Route::delete('/linhas/{id}/paradas', 'LinhaController@removeParadas')->name('removeParadas');
+        });
+
+        // endpoints de veiculos
+        Route::resource('/veiculos', 'VeiculoController');
+
+        // endpoints de posicaoVeiculos
+        Route::prefix('posicao-veiculos')->name('posicaoVeiculos.')->group(function() {
+            Route::get('/', 'PosicaoVeiculoController@index')->name('index');
+            //Route::post('/', 'PosicaoVeiculoController@store')->name('store');
+            Route::get('/{id}', 'PosicaoVeiculoController@show')->name('show');        
+            Route::put('/{id}', 'PosicaoVeiculoController@update')->name('update');                
+            //Route::delete('/{id}', 'PosicaoVeiculoController@destroy')->name('destroy');
+        });
     });
+
 });
+
