a39f59d4029b9e843d61e47be743670e12eed3e7
João Lapa <joaomanoel1996@hotmail.com>
2020-07-15 01:16:17 -0300

    Criadas annotations de documentação para o modelo

--- api-teste/app/User.php
+++ api-teste/app/User.php
@@ -5,14 +5,42 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 /**
- * Classe que representa o modelo de usuário
- * @OA/Schema
+ * @OA\Schema(
+ *     title="User",
+ *     description="model User",
+ *     @OA\Xml(
+ *         name="User"
+ *     )
+ * )
  */
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+
+    /**
+     * @OA\Property(
+     *     title="Name",
+     *     description="Nome do usuário",
+     *     example="Maria da Silva"
+     * )
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @OA\Property(
+     *     title="E-mail",
+     *     description="E-mail do usuário",
+     *     example="maria.silva@exemplo.com"
+     * )
+     *
+     * @var string
+     */
+    private $email;
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +68,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Obter o identificador que será armazenado na requisição do JWT    
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Retorna um array associativo contendo qualquer conteúdo personalizado a ser adicionado ao JWT
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
