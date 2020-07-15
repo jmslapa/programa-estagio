d58285a2dbf477830ddd06f33af64f50f267111d
João Lapa <joaomanoel1996@hotmail.com>
2020-07-14 22:37:43 -0300

    Criado método privado calcularDistanciaGeocordenadas, que recebe latitude e longitude e 2 posições gps e calcula a distância entre si. Criado método paradasProximas, que retorna todas as paradas até 500m de distância da posição recebida por parâmetro.

--- api-teste/app/Services/Api/SearchService.php
+++ api-teste/app/Services/Api/SearchService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Api;
 
 use App\Http\Resources\LinhaCollection;
+use App\Http\Resources\ParadaCollection;
 use App\Http\Resources\VeiculoCollection;
 use App\Linha;
 use App\Parada;
@@ -61,5 +62,62 @@ class SearchService
     {
         $linha = $this->linhaRepository->findById($id);
         return new VeiculoCollection($linha->veiculos);
+    }
+
+    /**
+     * Retorna todas as paradas que estejam até 500 metros da posição informada
+     *
+     * @param array $data
+     * @return PardaCollection
+     */
+    public function paradasProximas($data)
+    {
+        $from_lat = $data['latitude'];
+        $from_lon = $data['longitude'];
+
+        $paradasProximas = [];
+
+        foreach($this->paradaRepository->getAll() as $p) {
+            $dist = $this->calcularDiscanciaGeocordenadas($from_lat, $from_lon, $p->latitude, $p->longitude);
+            if($dist <= 500) {
+                $paradasProximas[] = [
+                    'parada' => $p,
+                    'distancia' => round($dist) . ' metros'
+                ];
+            }
+        }
+
+        return $paradasProximas;        
+    }
+
+    /**
+     * Recebe latitude e longitude de 2 posições e calcula a distância entre elas em metros.
+     *
+     * @param double $from_lat
+     * @param double $from_lon
+     * @param double $to_lat
+     * @param double $to_lon
+     * @return void
+     */
+    private function calcularDiscanciaGeocordenadas($from_lat, $from_lon, $to_lat, $to_lon)
+    {   
+        $d_lat = abs($from_lat - $to_lat);
+        $d_lon = abs($from_lon - $to_lon);
+
+        $dist = null;
+
+        if(!$d_lat || !$d_lon) {
+            if(!$d_lat) {
+                $dist = $d_lon*60*1852;
+            }else {
+                $dist = $d_lat*60*1852;
+            }
+        }else {
+            $c1 = $d_lat*60*1852;
+            $c2 = $d_lon*60*1852;
+            $dist = sqrt((pow($c1,2)) + pow($c2, 2));
+        }
+
+        return $dist;
     }
 }
