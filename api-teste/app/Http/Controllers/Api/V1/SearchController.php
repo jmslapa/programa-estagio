4c01d9a6db3997a2271791c77b0f384482b260a1
João Lapa <joaomanoel1996@hotmail.com>
2020-07-14 22:35:30 -0300

    Criado método paradasProximas

--- api-teste/app/Http/Controllers/Api/V1/SearchController.php
+++ api-teste/app/Http/Controllers/Api/V1/SearchController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Api\ApiMessage;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SearchParadasProximasRequest;
 use App\Services\Api\SearchService;
-
+use Illuminate\Http\Response;
 
 /**
  * @OA\Tag(
@@ -36,7 +37,7 @@ class SearchController extends Controller
      * @return \Illuminate\Http\Response
      * 
      *  @OA\Get(
-     *      path="/{parada_id}/linhas-por-parada",
+     *      path="/search/{parada_id}/linhas-por-parada",
      *      tags={"Search"},
      *      summary="Recupera linhas de uma parada",
      *      description="Retorna uma lista com todas as linhas de uma parada.",
@@ -55,9 +56,9 @@ class SearchController extends Controller
      *          @OA\JsonContent(ref="#/components/schemas/LinhaResource")
      *      ),      
      *      @OA\Response(
-     *          response=401,
-     *          ref="#/components/responses/401"
-     *      ),      
+     *          response=404,
+     *          ref="#/components/responses/404"
+     *      ),       
      *      @OA\Response(
      *          response=500,
      *          ref="#/components/responses/500"
@@ -83,7 +84,7 @@ class SearchController extends Controller
      * @return \Illuminate\Http\Response
      * 
      *  @OA\Get(
-     *      path="/{linha_id}/veiculos-por-linha",
+     *      path="/search/{linha_id}/veiculos-por-linha",
      *      tags={"Search"},
      *      summary="Recupera veículos de uma linha",
      *      description="Retorna uma lista com todos os veículos de uma linha.",
@@ -102,9 +103,9 @@ class SearchController extends Controller
      *          @OA\JsonContent(ref="#/components/schemas/VeiculoResource")
      *      ),      
      *      @OA\Response(
-     *          response=401,
-     *          ref="#/components/responses/401"
-     *      ),      
+     *          response=404,
+     *          ref="#/components/responses/404"
+     *      ),       
      *      @OA\Response(
      *          response=500,
      *          ref="#/components/responses/500"
@@ -116,6 +117,49 @@ class SearchController extends Controller
         try {
             
             $body = $this->service->veiculosPorLinha($id);
+            return response()->json($body, 200);
+            
+        }catch(\Exception $e) {
+            $message = new ApiMessage($e->getMessage());
+            return response()->json($message->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Retorna uma lista com todas as paradas até 500 metros da posição informada.
+     *
+     * @return \Illuminate\Http\Response
+     * 
+     *  @OA\Get(
+     *      path="/search/paradas-proximas",
+     *      tags={"Search"},
+     *      summary="Recupera paradas próximas",
+     *      description="Retorna uma lista com todas as paradas até 500 metros da posição informada",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/SearchParadasProximasRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operação bem-sucedida",
+     *          @OA\JsonContent(ref="#/components/schemas/ParadaResource")
+     *      ),      
+     *      @OA\Response(
+     *          response=404,
+     *          ref="#/components/responses/404"
+     *      ),     
+     *      @OA\Response(
+     *          response=422,
+     *          ref="#/components/responses/422"
+     *      )
+     *  )
+     */
+    public function paradasProximas(SearchParadasProximasRequest $request)
+    {
+        try {
+
+            $data = $request->all();            
+            $body = $this->service->paradasProximas($data);
             return response()->json($body, 200);
             
         }catch(\Exception $e) {
