55f8dc212c34ff1bf5d3ccbd9f791e27b8c8b401
João Lapa <joaomanoel1996@hotmail.com>
2020-07-16 19:04:57 -0300

    Criado método apra filtrar paradas

--- api-teste/app/Services/Api/SearchService.php
+++ api-teste/app/Services/Api/SearchService.php
@@ -37,6 +37,39 @@ class SearchService
         $this->paradaRepository = new ParadaRepository(new Parada());
         $this->linhaRepository = new LinhaRepository(new Linha());
     }
+
+    /**
+     * Recebe um array de modelos Linha e retorna uma LinhaCollection
+     *
+     * @param $collection
+     * @return LinhaCollection
+     */
+    public function linhaCollection($collection)
+    {
+        return new LinhaCollection($collection);
+    }
+
+    /**
+     * Recebe um array de modelos Parada e retorna uma ParadaCollection
+     *
+     * @param $collection
+     * @return ParadaCollection
+     */
+    public function paradaCollection($collection)
+    {
+        return new ParadaCollection($collection);
+    }
+
+    /**
+     * Recebe um array de modelos Veiculo e retorna uma VeiculoCollection
+     *
+     * @param $collection
+     * @return LinhaCollection
+     */
+    public function VeiculoCollection($collection)
+    {
+        return new VeiculoCollection($collection);
+    }
     
     /**
      * Retorna todas as linhas de uma parada.
@@ -47,7 +80,7 @@ class SearchService
      */
     public function linhasPorParada($id)
     {        
-        return new LinhaCollection($this->paradaRepository->getLinhas($id));
+        return $this->linhaCollection($this->paradaRepository->getLinhas($id));
     }
 
     /**
@@ -59,7 +92,7 @@ class SearchService
      */
     public function veiculosPorLinha($id)
     {
-        return new VeiculoCollection($this->linhaRepository->getVeiculos($id));
+        return $this->veiculoCollection($this->linhaRepository->getVeiculos($id));
     }
 
     /**
@@ -70,6 +103,26 @@ class SearchService
      */
     public function paradasProximas($data)
     {
-        return new ParadaCollection($this->paradaRepository->getParadasProximas($data));  
+        return $this->paradaCollection($this->paradaRepository->getParadasProximas($data));  
+    }
+
+    /**
+     * Recebe um array com uma query string e transforma o parâmetro 
+     * filters em um array de sub query strings utilizando o divisor ';'.
+     * 
+     * $exemplo $data['filter'] = 'name:like:%parada%;id:>:0' | ['name:like:%parada%', 'id:>:0']
+     * @param array $data
+     * @return Parada[]
+     */
+    public function filtrarParadas($data)
+    {   
+        $filters = $data['filters'] ?? null;
+        if($filters){            
+            $filters = explode(';', $data['filters']);
+            return $this->paradaCollection($this->paradaRepository->filtrar($filters));
+        }
+        else {
+            return $this->paradaCollection($this->paradaRepository->getAll());
+        }
     }
 }
