3b4a6867aa9ae63361da4610b2f93155727cbd19
João Lapa <joaomanoel1996@hotmail.com>
2020-07-16 19:04:37 -0300

    Criado método para filtrar paradas.

--- api-teste/app/Http/Controllers/Api/V1/SearchController.php
+++ api-teste/app/Http/Controllers/Api/V1/SearchController.php
@@ -6,6 +6,7 @@ use App\Api\ApiMessage;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SearchParadasProximasRequest;
 use App\Services\Api\SearchService;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 /**
@@ -37,7 +38,7 @@ class SearchController extends Controller
      * @return \Illuminate\Http\Response
      * 
      *  @OA\Get(
-     *      path="/search/{parada_id}/linhas-por-parada",
+     *      path="/search/paradas/{parada_id}/linhas-por-parada",
      *      tags={"Search"},
      *      summary="Recupera linhas de uma parada",
      *      description="Retorna uma lista com todas as linhas de uma parada.",
@@ -84,7 +85,7 @@ class SearchController extends Controller
      * @return \Illuminate\Http\Response
      * 
      *  @OA\Get(
-     *      path="/search/{linha_id}/veiculos-por-linha",
+     *      path="/search/linhas/{linha_id}/veiculos-por-linha",
      *      tags={"Search"},
      *      summary="Recupera veículos de uma linha",
      *      description="Retorna uma lista com todos os veículos de uma linha.",
@@ -132,7 +133,7 @@ class SearchController extends Controller
      * @return \Illuminate\Http\Response
      * 
      *  @OA\Get(
-     *      path="/search/paradas-proximas",
+     *      path="/search/paradas/paradas-proximas",
      *      tags={"Search"},
      *      summary="Recupera paradas próximas",
      *      description="Retorna uma lista com todas as paradas até 500 metros da posição informada",
@@ -161,6 +162,56 @@ class SearchController extends Controller
 
             $data = $request->all();            
             $body = $this->service->paradasProximas($data);
+            return response()->json($body, 200);
+            
+        }catch(\Exception $e) {
+            $message = new ApiMessage($e->getMessage());
+            return response()->json($message->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Retorna uma lista com todas as paradas até 500 metros da posição informada.
+     * 
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     * 
+     *  @OA\Get(
+     *      path="/search/paradas/filtrar",
+     *      tags={"Search"},
+     *      summary="Filtra Paradas",
+     *      description="Retorna uma lista de paradas com filtros aplicados",
+     *      @OA\Parameter(
+     *          name="filters",
+     *          description="Filtros de busca no formato 'atributo:operador:parâmetro' com ';' como delimitador",
+     *          required=false,
+     *          in="query",
+     *          example="?filters=name:like:%parada%;id:>:0",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operação bem-sucedida",
+     *          @OA\JsonContent(ref="#/components/schemas/ParadaResource")
+     *      ),      
+     *      @OA\Response(
+     *          response=404,
+     *          ref="#/components/responses/404"
+     *      ),     
+     *      @OA\Response(
+     *          response=422,
+     *          ref="#/components/responses/422"
+     *      )
+     *  )
+     */
+    public function filtrarParadas(Request $request)
+    {
+        try {
+
+            $data = $request->all();
+            $body = $this->service->filtrarParadas($data);
             return response()->json($body, 200);
             
         }catch(\Exception $e) {
